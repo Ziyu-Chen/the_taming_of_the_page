013d66f158efc9b7faa46aef4e654c239860f660
Ziyu Chen <zc839@nyu.edu>
2020-05-14 23:40:23 +0800

    Added the scrolling functionality.

--- popup.js
+++ popup.js
@@ -4,18 +4,26 @@
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+let scrollButton = document.getElementById('scroll');
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+const keepScrollingCode = "const sleep = (ms) => {return new Promise(resolve => setTimeout(resolve, ms));};const scroll = async () => {let position = 0;while (true) {await sleep(1000);window.scroll(0, position);if (position > document.body.scrollHeight) {position = 0;} else {position += 100;};};}; scroll();"
+
+
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+
+scrollButton.onclick = function() {
+  chrome.tabs.query({currentWindow: true}, function(tabs) {
+    for (const tab of tabs) {
+      console.log(tab)
+      chrome.tabs.executeScript(
+        tab.id,
+        {code: keepScrollingCode}
+      );
+    }
   });
 };
+
